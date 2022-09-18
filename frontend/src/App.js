88d8baae16bb2a0c302c0a6ae021b92dd2c40a60
Shruti Gupta <shrutigupta5555@gmail.com>
2022-09-18 14:40:32 +0530

    add router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
-     <h1>dummy</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
