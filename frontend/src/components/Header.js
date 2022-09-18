00a44df850f632a960f7c49402d13af8ae3f902c
Shruti Gupta <shrutigupta5555@gmail.com>
2022-09-18 19:59:00 +0530

    maintain user logged in state

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import {NavLink, useHistory} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {NavLink, useHistory, useNavigate} from 'react-router-dom';
 import './Header.css'
+import { checkLoggedIn, userDetails } from '../utils/checkLoggedIn';
 
 const Header = () => {
+
+    const navigate = useNavigate()
+    const [user , setUser ] = useState(null)
+
+    useEffect(()=> {
+      if(checkLoggedIn()){
+          console.log(userDetails())
+          setUser(userDetails())
+      }else{
+          navigate('/welcome')
+      }
+    }, [])
+    
   return (
     <div className='navbar'>
         <div className="logo">
@@ -12,7 +26,8 @@ const Header = () => {
             <NavLink end activeClassName="active" to="/">Home</NavLink>
             <NavLink end activeClassName="active" to="/dashboard">Dashboard</NavLink>
             <NavLink end activeClassName="active" to="/profile">Profile</NavLink>
-            <NavLink end activeClassName="active" to="/login">Login</NavLink>
+            {user? <span activeClassName="inactive">{user.email}</span> : <NavLink end activeClassName="active" to="/login">Login</NavLink>}
+            
         </div>
     </div>
   )
--- frontend/src/utils/checkLoggedIn.js
+++ frontend/src/utils/checkLoggedIn.js
@@ -1,17 +1,25 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { initialise } from "./util";
 
 
 
 export const checkLoggedIn = () => {
 
-
+    initialise()
     const auth = getAuth();
-    let ifLoggedIn = false;
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const uid = user.uid;
-        ifLoggedIn = true;
-      } 
-    });
-    return ifLoggedIn;
+    
+    if(auth.currentUser){
+        return true
+    }else {
+        return false
+    }
+    
+}
+
+
+export const userDetails = () => {
+    initialise()
+    const auth = getAuth();
+    
+    return auth.currentUser;
 }
